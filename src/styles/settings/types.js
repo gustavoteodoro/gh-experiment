4a362d1abac4879545cd05bfb35379dc8ad5547a
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2017-11-11 00:25:52 -0200

    feat: Importing fonts

---
+++ src/styles/settings/types.js
@@ -0,0 +1,29 @@
+export function oswaldRegular(fontSize) {
+    return `
+        font-family: 'Oswald', sans-serif;
+        font-size: ${fontSize};
+        font-weight: 400;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    `;
+}
+
+export function ptSansRegular(fontSize) {
+    return `
+        font-family: 'PT Sans', sans-serif;
+        font-size: ${fontSize};
+        font-weight: 400;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    `;
+}
+
+export function ptSansBold(fontSize) {
+    return `
+        font-family: 'PT Sans', sans-serif;
+        font-size: ${fontSize};
+        font-weight: 700;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    `;
+}
